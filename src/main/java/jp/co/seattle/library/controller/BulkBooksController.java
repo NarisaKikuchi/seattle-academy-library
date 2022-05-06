195c44c200c3700b5ee6745ec155f2eac9d9644b
Narisa Kikuchi <narisakikuchi@NarisanoMacBook-Air.local>
2022-05-06 18:05:44 +0900

    タスク８説明文の追加

--- src/main/java/jp/co/seattle/library/controller/BulkBooksController.java
+++ src/main/java/jp/co/seattle/library/controller/BulkBooksController.java
@@ -92,6 +92,7 @@ public class BulkBooksController {
 				bookInfo.setPublisher(bookList[2]);
 				bookInfo.setPublishDate(bookList[3]);
 				bookInfo.setISBN(bookList[4]);
+				bookInfo.setDescription(bookList[5]);
 
 				// 書籍情報に一括登録する
 				booksService.registBook(bookInfo);
